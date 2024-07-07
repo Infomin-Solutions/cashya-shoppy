eb545d0892ed4b1a53a2906c1bdbf51df4b71e5e
Lamin Muhammed M <laminmuhammed@gmail.com>
2024-07-08 01:23:29 +0530

    added site settings models

--- root/admin.py
+++ root/admin.py
@@ -1,3 +1,20 @@
+import sys
+from . import models
 from django.contrib import admin
 
 # Register your models here.
+
+
+if 'makemigrations' not in sys.argv and 'migrate' not in sys.argv and print(1) is None and models.SiteSetting.objects.count() == 0:
+    models.SiteSetting.objects.create()
+
+
+@admin.register(models.SiteSetting)
+class SiteSettingAdmin(admin.ModelAdmin):
+    list_display = ['site_name']
+
+    def has_add_permission(self, request):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
--- root/models.py
+++ root/models.py
@@ -1,3 +1,11 @@
+import sys
 from django.db import models
 
 # Create your models here.
+
+
+class SiteSetting(models.Model):
+    site_name = models.CharField(max_length=255, default='Cashya Shoppy')
+
+    def __str__(self):
+        return self.site_name
