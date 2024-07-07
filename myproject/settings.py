12c4e574d2c20ff36dee7b1ebd930183f83737e0
Lamin Muhammed M <laminmuhammed@gmail.com>
2024-07-08 00:55:00 +0530

    created a new root app

--- myproject/settings.py
+++ myproject/settings.py
@@ -51,10 +51,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'corsheaders',
     'rest_framework',
     'rest_framework_simplejwt',
-    'corsheaders',
+
     'ecom',
+    'root'
 ]
 
 MIDDLEWARE = [
